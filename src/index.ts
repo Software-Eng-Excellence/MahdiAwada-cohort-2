589f3cbd37aaeea1d899bd113bf05507550b65de
Mahdi Awada <awadamahdi37@gmail.com>
2025-06-07 00:29:34 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import logger from "./util/logger";
+import { readCSVFile } from "./util/parser";
+
+async function main() {
+    const data = await readCSVFile("src/data/cake orders.csv");
+    // for each data row, log the row
+    data.forEach( row => logger.info(row)); 
+}
+
+main()
