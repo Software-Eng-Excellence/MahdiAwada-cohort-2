0b371f1baa6445f150ef92cd4a6b7410cf4ce8ef
Mahdi Awada <awadamahdi37@gmail.com>
2025-06-18 19:09:43 +0300

    Added a Cake Builder

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
-import logger from "./util/logger";
-import { readCSVFile } from "./util/parsers/csvParser";
-import { parse_json_file } from './util/parsers/jsonParser';
-import { parse_xml_file } from "./util/parsers/xmlParser";
+import { CakeBuilder } from "./model/builders/cake.builder";
 
 async function main() {
 
-    // CSV data
-    const csvData = await readCSVFile("src/data/cake orders.csv");
-    csvData.forEach( row => logger.info(`CSV data: ${row}`)); 
+    const cakeBuilder = new CakeBuilder();
 
-    // JSON data
-    const jsonData = await parse_json_file("./src/data/book orders.json");
-    jsonData.forEach( (row: object) => logger.info(`JSON data: ${row}`));
-
-    //XML data
-    const xmlData = await parse_xml_file("./src/data/toy orders.xml");
-    const rows = xmlData.data.row;
-    rows.forEach((row: object) => logger.info(`XML data: ${row}`));
+    const cake = cakeBuilder
+        .setType("type")
+        .setFlavor("flavor")
+        .setFilling("filling")
+        .setSize(10)
+        .setLayers(2)
+        .setFrostingType("frostingType")
+        .setFrostingFlavor("frostingFlavor")
+        .setDecorationType("decorationType")
+        .setDecorationColor("decorationColor")
+        .setCustomMessage("customMessage")
+        .setShape("shape")
+        .setAllergies("allergies")
+        .setSpecialIngredients("specialIngredients")
+        .setPackagingType("packagingType")
+        .build();
+    console.log(cake);
 }
 
 main()
---
+++ src/model/builders/cake.builder.ts
@@ -0,0 +1,132 @@
+import logger from "../../util/logger";
+import { Cake } from "../Cake.model";
+
+export class CakeBuilder {
+    private type!: string;
+    private flavor!: string;
+    private filling!: string;
+    private size!: number;
+    private layers!: number;
+    private frostingType!: string;
+    private frostingFlavor!: string;
+    private decorationType!: string;
+    private decorationColor!: string;
+    private customMessage!: string;
+    private shape!: string;
+    private allergies!: string;
+    private specialIngredients!: string;
+    private packagingType!: string;
+
+    public setType(type: string): CakeBuilder {
+        this.type = type;
+        return this;
+    }
+    public setFlavor(flavor: string): CakeBuilder {
+        this.flavor = flavor;
+        return this;
+    }
+
+    public setFilling(filling: string): CakeBuilder {
+        this.filling = filling;
+        return this;
+    }
+
+    public setSize(size: number): CakeBuilder {
+        this.size = size;
+        return this;
+    }
+
+    public setLayers(layers: number): CakeBuilder {
+        this.layers = layers;
+        return this;
+    }
+
+    public setFrostingType(frostingType: string): CakeBuilder {
+        this.frostingType = frostingType;
+        return this;
+    }
+
+    public setFrostingFlavor(frostingFlavor: string): CakeBuilder {
+        this.frostingFlavor = frostingFlavor;
+        return this;
+    }
+
+    public setDecorationType(decorationType: string): CakeBuilder {
+        this.decorationType = decorationType;
+        return this;
+    }
+
+    public setDecorationColor(decorationColor: string): CakeBuilder {
+        this.decorationColor = decorationColor;
+        return this;
+    }
+
+    public setCustomMessage(customMessage: string): CakeBuilder {
+        this.customMessage = customMessage;
+        return this;
+    }
+
+    public setShape(shape: string): CakeBuilder {
+        this.shape = shape;
+        return this;
+    }
+
+    public setAllergies(allergies: string): CakeBuilder {
+        this.allergies = allergies;
+        return this;
+    }
+
+    public setSpecialIngredients(specialIngredients: string): CakeBuilder {
+        this.specialIngredients = specialIngredients;
+        return this;
+    }
+
+    public setPackagingType(packagingType: string): CakeBuilder {
+        this.packagingType = packagingType;
+        return this;
+    }
+    
+
+    build(): Cake {
+        const requiredProperties = [
+            this.type,
+            this.flavor,
+            this.filling,
+            this.size,
+            this.layers,
+            this.frostingType,
+            this.frostingFlavor,
+            this.decorationType,
+            this.decorationColor,
+            this.customMessage,
+            this.shape,
+            this.allergies,
+            this.specialIngredients,
+            this.packagingType
+        ];
+
+        for ( const property of requiredProperties ) {
+            if (!property) {
+                logger.error("Missing required properties, could not build a cake");
+                throw new Error("Missing required properties");
+            }
+        }
+
+        return new Cake(
+            this.type,
+            this.flavor,
+            this.filling,
+            this.size,
+            this.layers,
+            this.frostingType,
+            this.frostingFlavor,
+            this.decorationType,
+            this.decorationColor,
+            this.customMessage,
+            this.shape,
+            this.allergies,
+            this.specialIngredients,
+            this.packagingType
+        )
+    }
+}
